67425f57ae173f64533c7aa5ef173d84d364be59
ananasik558 <denchickshow@list.ru>
2023-03-25 14:43:11 +0300

    1

--- Lab.c
+++ Lab.c
@@ -1,152 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct tree{
-    int key;
-    struct tree*child;
-    struct tree*sibling;
-    struct tree*parent;
-    struct tree*left;
-    struct tree*right;
+#define Max_Size 100
+
+struct stack
+{
+    int elem[Max_Size];
+    int top;
 };
-typedef struct tree node;
 
-int getWidth(node* t, int level);
-int height(node* t);
-
-int check(node* t) {
-    int h = height(t);
-    int width, lastWidth;
-    for(int i = 1; i <= h; i++) {
-        width = getWidth(t, i);
-        if(width > lastWidth) {
-            return 0;
-        }
-        lastWidth = width;
-    }
-    return 1;
+void init(struct stack * stk) {
+    stk->top = 0;
 }
 
-int getWidth(node* t, int level) {
-    if(t == NULL) {
-        return 0;
-    }
-    if(level == 1) {
+void push(struct stack * stk, int f) {
+        stk->elem[stk->top] = f;
+        stk->top++;
+}
+
+int getcount(struct stack *stk) {
+    return stk->top;
+}
+
+int empty(struct stack *stk) {
+    if(stk->top == 0) {
         return 1;
     }
-    if(level > 1) {
-        return getWidth(t->left, level - 1) + getWidth(t->right, level - 1);
-    }
+    return 0;
 }
 
-int height(node* t) {
-    if(t == NULL) {
-        return 0;
-    } else {
-        int lHeight = height(t->left);
-        int rHeight = height(t->right);
-        return (lHeight > rHeight) ? (lHeight + 1) : (rHeight + 1);
-    }
-}
-
-node *create(int parent) {
-    node *tree = (node *) malloc(sizeof(node));
-    tree->key = parent;
-    tree->child = NULL;
-    tree->sibling = NULL;
-    tree->parent = NULL;
-    return tree;
-}
-
-node *find_node(node* tree, int parent) {
-    if (tree == NULL) {
-        return NULL;
-    }
-    if (tree->key == parent) {
-        return tree;
-    }
-    node* child = NULL;
-    if (tree->child) {
-        child = find_node(tree->child, parent);
-        if (child != NULL) {
-            return child;
-        }
-    }
-    if (tree->sibling) {
-        child = find_node(tree->sibling, parent);
-        if (child != NULL) {
-            return child;
-        }
-    }
-}
-
-void add(node* tree, int child, int parent) {
-    node *res = find_node(tree, child);
-    if (res == NULL) {
+void print(struct stack *stk) {
+    int i;
+    i = stk->top;
+    if(empty(stk) == 1) {
         return;
     }
-    node *res_parent = res;
-    if (res->child == NULL) {
-        res->child = create(parent);
-        res->child->parent = res;
-        return;
-    }
-    res = res->child;
-    while (res->sibling != NULL) {
-        res = res->sibling;
-    }
-    res->sibling = create(parent);
-    res->sibling->parent = res_parent;
-}
-
-
-void tree_show(node *child, int deep) {
-    if (child != NULL) {
-        for (int i = 0; i < deep; i++) {
-            printf("    ");
-        }
-        printf("%d\n", child->key);
-        if (child->child != NULL) {
-            tree_show(child->child, deep + 1);
-        }
-        tree_show(child->sibling, deep);
+    while(i > 0) {
+        i--;
+        printf("%d\n", stk->elem[i]);
     }
 }
 
-void tree_print(node *t) {
-    tree_show(t, 0);
+int pop(struct stack *stk) {
+    int elem;
+    if(stk->top > 0) {
+        stk->top--;
+        elem = stk->elem[stk->top];
+        return elem;
+    }
 }
 
-
-void tree_destroy(node *t) {
-    if (t != NULL) {
-        if (t->child != NULL) {
-            tree_destroy(t->child);
+void sort(int* s_arr, int first, int last)
+{
+    int i = first, j = last, x = s_arr[(first + last) / 2];
+  
+    do {
+        while (s_arr[i] < x) i++;
+        while (s_arr[j] > x) j--;
+  
+        if(i <= j) {
+            if (s_arr[i] > s_arr[j]) {
+                int s_arr1 = s_arr[i];
+                s_arr[i] = s_arr[j];
+                s_arr[j] = s_arr1;
+            }
+            i++;
+            j--;
         }
-        if (t->sibling != NULL) {
-            tree_destroy(t->sibling);
-        }
-    }
-    free(t);
+    } while (i <= j);
+  
+    if (i < last)
+        sort(s_arr, i, last);
+    if (first < j)
+        sort(s_arr, first, j);
 }
 
 int main() {
-    int root, count = 0;
-    node *t = NULL;
-    scanf("%d", &count);
-    scanf("%d", &root);
-    t = create(root);
-    for(int i = 0; i < count - 1; ++i) {
-        int parent, child;
-        scanf("%d%d", &child, &parent);
-        add(t, child, parent);
+    struct stack * stk;
+    int len1, len2;
+    int elem;
+    stk = (struct stack*)malloc(sizeof(struct stack));
+    init(stk);
+    printf("Введите число элементов стека первого\n");
+    scanf("%d", &len1);
+    printf("Введите элементы:");
+    for(int i = 0; i < len1; i++) {
+        scanf("%d", &elem);
+        push(stk, elem);
     }
-    if (check(t)) {
-        printf("YES\n");
-    } else {
-        printf("NO\n");
+    printf("Введите число элементов стека второго\n");
+    scanf("%d", &len2);
+    printf("Введите элементы:");
+    for(int i = 0; i < len2; i++) {
+        scanf("%d", &elem);
+        push(stk, elem);
     }
-    tree_print(t);
-    tree_destroy(t);
+    print(stk);
+    printf("\n");
+    int mas[getcount(stk)], n = getcount(stk);
+    for(int i = 0; i < len1 + len2; i++) {
+        mas[i] = pop(stk);
+
+    }
+    sort(mas, 0, n - 1);
+    printf("\n");
+    for(int i = 0; i < len1 + len2; i++) {
+        push(stk, mas[i]);
+    }
+    print(stk);
     return 0;
 }
